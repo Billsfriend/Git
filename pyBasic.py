288100581e57056cbc55d57cac0fb94a20103f36
Bill Gao <Billsfriend1999@outlook.com>
2020-03-26 20:33:52 +0800

    add section 'string and code'

--- pyBasic.py
+++ pyBasic.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # this lesson is based on python 3.8
 # ====BASIC I/O====
 print('hello world') # basic output: print(). use '' or "" to fold string
@@ -22,9 +25,9 @@ print('first line \n second line') # \n as escape character, line continuation
 print(r'first line \n second line') # in `r''`, cancel any special escape character
 
 print('''If you miss the train I'm on
-... you will know that I'm gone
-... you can hear the whistle blow
-... one hundred miles''') # use '''...''' to note multiple lines
+you will know that I'm gone
+you can hear the whistle blow
+one hundred miles''') # use '''...''' to note multiple lines
 
 
 # ====VARIABLE AND TYPE====
@@ -54,5 +57,16 @@ print(4/2) # print `2.0`.
 print(5//2) # print `2`. this division operator `//` always lead to integer result, like in C
 print(5%1) # print `1`. `%` gets the left number
 
+# ====STRINGS AND ENCODE====
 
+print('Python3以Unicode编码，所以支持中文')
+print(ord('0')) # print `48`. ord() return unicode code of a one-character string
+print(chr(48)) # print '0'. chr() do the opposite of ord()
 
+print('\u4e2d\u6587') # print '中文'. you can use \+unicode to print string
+
+'abc'.encode('ascii') # print b'abc'. use .encode() to encode a string to bytes form for transmission and storage
+'中文'.encode('utf-8') # print b'\xe4\xb8\xad\xe6\x96\x87'. in bytes form (b'').
+b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8') # print '中文'. use .decode() to transform bytes back to string
+
+print('hello, %s'% 'bill') # use % to format. %s for string, %d for integer
