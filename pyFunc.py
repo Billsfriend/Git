5aba05b008252621fda158ec6a0fcaf9fc704367
Bill Gao <Billsfriend1999@outlook.com>
2020-04-06 17:06:42 +0800

    'start function lesson. list the synopsis'

---
+++ pyFunc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# this lesson is based on python 3
+
+# ====CALL A FUNCTION====
+
+
+
+# ====DEFINE A FUNCTION====
+
+
+
+# ====ARGUMENTS====
+
+
+
+# ====RECURSIVE====
+
+
+
